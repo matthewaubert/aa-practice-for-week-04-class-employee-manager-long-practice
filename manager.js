972db90bce96648bc417916efa878b0edb16f1b3
matthewaubert <132402803+matthewaubert@users.noreply.github.com>
2023-07-19 19:05:51 -0400

    Phase 5: Create local test

--- manager.js
+++ manager.js
@@ -101,15 +101,15 @@ After:  Manager {
 */
 
 
-const splinter = new Manager('Splinter', 100000, 'Sensei');
-const leo = new Manager('Leonardo', 90000, 'Ninja', splinter);
-const raph = new Manager('Raphael', 90000, 'Ninja', leo);
-const mikey = new Employee('Michelangelo', 85000, 'Grasshopper', raph);
-const donnie = new Employee('Donatello', 85000, 'Grasshopper', raph);
+// const splinter = new Manager('Splinter', 100000, 'Sensei');
+// const leo = new Manager('Leonardo', 90000, 'Ninja', splinter);
+// const raph = new Manager('Raphael', 90000, 'Ninja', leo);
+// const mikey = new Employee('Michelangelo', 85000, 'Grasshopper', raph);
+// const donnie = new Employee('Donatello', 85000, 'Grasshopper', raph);
 
-console.log(splinter.calculateBonus(0.05)); // => 22500
-console.log(leo.calculateBonus(0.05)); // => 17500
-console.log(raph.calculateBonus(0.05)); // => 13000
+// console.log(splinter.calculateBonus(0.05)); // => 22500
+// console.log(leo.calculateBonus(0.05)); // => 17500
+// console.log(raph.calculateBonus(0.05)); // => 13000
 
 
 /****************************** EXPORT ******************************/
